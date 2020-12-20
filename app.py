f182a39f50c63dbd1475e4e77af9b42df2077632
ramborell23 <jerelldavis@ac.c4q.nyc>
2020-12-19 23:54:54 -0500

    added new submit route

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, request,  render_template
 app = Flask(__name__)
-from flask import render_template
 
 
 @app.route('/')
-def hello(name='Kam'):
+def hello(name=None):
+    form_name = request.form['name']
     return render_template('hello.html', name=name)
+
+
+@app.route('/handle_name_submit', methods=['POST'])
+def handle_data():
+    form_name = request.form['name']
+    return render_template('hello.html', name=form_name)
+
+
+@app.route('/handle_madlibs_data', methods=['POST'])
+def handle_data():
+    form_verb = request.form['verb']
+    form_adverb = request.form['adverb']
+    form_adjective = request.form['adjective']
+    form_noun = request.form['noun']
+
+    return render_template('hello.html', name=form_verb)
+
 
 if __name__ == '__main__':
     app.run()
